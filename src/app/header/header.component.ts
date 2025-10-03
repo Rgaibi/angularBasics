8bd86434b064cda611184baeb65492399f5dc7d5
karim rgaibi <karimrgaibi@gmail.com>
2025-10-03 18:06:12 +0200

    view template of component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'app-header',
-    template : '<h3>eCom</h3>'
+    templateUrl : './header.component.html'
 })
 export class HeaderComponent {
 
