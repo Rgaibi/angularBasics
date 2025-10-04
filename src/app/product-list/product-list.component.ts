3e3a025a3e8da03e79614325ed0f6364eb06f461
karim rgaibi <karimrgaibi@gmail.com>
2025-10-04 22:29:34 +0200

    Data Binding : String interpolation

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  product = {
+    name : 'iPhone X',
+    price : 789,
+    color : 'Black',
+    discount : 10,
+    inStock : 5
+  }
+  getDiscountedPrice() {
+    return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+  
+
+}
