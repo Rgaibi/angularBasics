618d852d9e107405dd5e892f4dda4b43942064c3
karim rgaibi <karimrgaibi@gmail.com>
2025-10-07 21:31:42 +0200

    Input Decorator

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -533,4 +533,11 @@ export class ProductListComponent {
     }
   ];
 
+  totalProductCount = this.products.length;
+  totalProductInStock = this.products.filter(p => p.is_in_inventory ===true).length;
+  totalProductOutOfStock = this.products.filter(p => p.is_in_inventory ===false).length;
+
+
 }
+
+
