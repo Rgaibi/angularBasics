8b8a4cf39eae8dffb32e103dc70bd99eb91ce3c9
karim rgaibi <karimrgaibi@gmail.com>
2025-10-14 20:46:11 +0200

    Renderer2

--- src/app/CustomDirectives/SetColor.directive.ts
+++ src/app/CustomDirectives/SetColor.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[setColor]'
 })
 export class SetColor implements OnInit {
     // private element: ElementRef;
-    constructor(private element: ElementRef) { 
+    constructor(private element: ElementRef,private Renderer: Renderer2) { 
         // this.element = element;
     }
 
     ngOnInit() {
-        this.element.nativeElement.style.color = '#000000ff';       
-        this.element.nativeElement.style.fontWeight = 'bold';        
+        // this.element.nativeElement.style.color = '#000000ff';       
+        // this.element.nativeElement.style.fontWeight = 'bold';
+        this.Renderer.setStyle(this.element.nativeElement, 'color','#000000ff');
+                this.Renderer.setStyle(this.element.nativeElement, 'fontWeight','bold');        
+                // this.Renderer.setAttribute(this.element.nativeElement, 'title','exemple of setAttribute');         
     }
 }
